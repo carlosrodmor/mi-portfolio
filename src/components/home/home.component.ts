5fb1ccfe76665140aa73c63dcb17f918fa7b8dcc
Carlos <carlos97sdg@gmail.com>
2024-08-07 21:51:24 +0200

    update home component to use fade-in scroll directive

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FadeInOnScrollDirective } from '../../directives/fade-in-scroll.directive';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FadeInOnScrollDirective],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
