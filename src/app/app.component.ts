d78ca3579c02aaffbf8ce915d9af3e960a389819
Carlos <carlos97sdg@gmail.com>
2024-08-07 19:11:35 +0200

    remove contact component and update app component imports
    later, working in this branch in the home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from "../components/home/home.component";
-import { AboutComponent } from "../components/about/about.component";
-import { ProjectsComponent } from "../components/projects/projects.component";
-import { ContactComponent } from "../components/contact/contact.component";
-import { FooterComponent } from "../components/footer/footer.component";
-import { NavbarComponent } from "../components/navbar/navbar.component";
+import { HomeComponent } from '../components/home/home.component';
+import { AboutComponent } from '../components/about/about.component';
+import { ProjectsComponent } from '../components/projects/projects.component';
+import { FooterComponent } from '../components/footer/footer.component';
+import { NavbarComponent } from '../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HomeComponent, AboutComponent, ProjectsComponent, ContactComponent, FooterComponent, NavbarComponent],
+  imports: [
+    CommonModule,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    FooterComponent,
+    NavbarComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
