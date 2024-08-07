429ced6b9f2a008589e51fb891ad9784ebccf59a
Carlos <carlos97sdg@gmail.com>
2024-08-07 19:41:37 +0200

    dd real-time clock (madrid) to navbar component

--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -42,12 +42,15 @@ export class NavbarComponent implements OnInit, OnDestroy {
 
   updateTime() {
     const now = new Date();
-    this.currentTime = now.toLocaleTimeString('en-US', {
+    const madridTime = new Intl.DateTimeFormat('en-US', {
+      timeZone: 'Europe/Madrid',
       hour: '2-digit',
       minute: '2-digit',
       second: '2-digit',
       hour12: false,
-    });
+    }).format(now);
+
+    this.currentTime = madridTime;
   }
 
   scrollTo(sectionId: string) {
