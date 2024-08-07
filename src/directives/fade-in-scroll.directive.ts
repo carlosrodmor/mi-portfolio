87c821cfb24f1447653f7216ef8e5eecf7a9c09a
Carlos <carlos97sdg@gmail.com>
2024-08-07 19:35:54 +0200

    update app component to use fade-in scroll directive

--- src/directives/fade-in-scroll.directive.ts
+++ src/directives/fade-in-scroll.directive.ts
@@ -1,16 +1,34 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  Inject,
+  PLATFORM_ID,
+} from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Directive({
   selector: '[appFadeInOnScroll]',
   standalone: true,
 })
 export class FadeInOnScrollDirective implements OnInit {
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
   ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.setupIntersectionObserver();
+    }
+  }
+
+  private setupIntersectionObserver() {
     const options = {
       root: null,
-      rootMargin: window.innerWidth < 768 ? '0px 0px -50px 0px' : '0px',
+      rootMargin: '0px',
       threshold: 0.1,
     };
 
