8b1bda22d4e7adac6503d81192f67f0671c2c01c
Carlos Rodríguez <carlos97sdg@gmail.com>
2024-08-27 18:31:13 +0200

    navbar and footer excluded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../components/navbar/navbar.component';
 import { FooterComponent } from '../components/footer/footer.component';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [NavbarComponent, FooterComponent, RouterOutlet, CommonModule],
 })
-export class AppComponent {}
+export class AppComponent {
+  showNavbarFooter: boolean = true;
+
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.router.events.subscribe(() => {
+      this.checkRoute();
+    });
+  }
+
+  checkRoute() {
+    const currentRoute = this.router.url;
+    // Añade aquí las rutas en las que no quieres mostrar la navbar y el footer
+    const excludedRoutes = ['/cv'];
+    this.showNavbarFooter = !excludedRoutes.includes(currentRoute);
+  }
+}
